055bf2e6b3aef17334ea06a649a8c518219702a6
Rutul Shah <rutulshah17@gmail.com>
2019-11-07 08:56:12 -0500

    Updates

---
+++ precache-manifest.ddb5bdf4f56c8ec46c7a73e8d14f6f56.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b235031264588cce1d635cb51bc34cd3",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c2dfb29263818e1c26b8",
+    "url": "/monsters-rolodex/static/css/main.4a4e9359.chunk.css"
+  },
+  {
+    "revision": "6df4eb5567d793ed81f5",
+    "url": "/monsters-rolodex/static/js/2.975c5e1d.chunk.js"
+  },
+  {
+    "revision": "c2dfb29263818e1c26b8",
+    "url": "/monsters-rolodex/static/js/main.3912a9d9.chunk.js"
+  },
+  {
+    "revision": "f56f2e2c6d96b014d91f",
+    "url": "/monsters-rolodex/static/js/runtime-main.86190389.js"
+  }
+]);
