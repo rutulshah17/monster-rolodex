c67892152057d5db0cd120a63a1ca82d904d5a0e
Rutul Shah <rutulshah17@gmail.com>
2021-02-05 07:28:44 -0500

    Updates

--- precache-manifest.805a1959d4ec875ad2fd412f5d72f7b7.js
+++ precache-manifest.805a1959d4ec875ad2fd412f5d72f7b7.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b235031264588cce1d635cb51bc34cd3",
+    "revision": "53bb0c5db5ef27ba693f02abb7e2b710",
     "url": "/monsters-rolodex/index.html"
   },
   {
-    "revision": "c2dfb29263818e1c26b8",
-    "url": "/monsters-rolodex/static/css/main.4a4e9359.chunk.css"
+    "revision": "f365428c553241d58ad1",
+    "url": "/monsters-rolodex/static/css/main.bafea802.chunk.css"
   },
   {
     "revision": "6df4eb5567d793ed81f5",
     "url": "/monsters-rolodex/static/js/2.975c5e1d.chunk.js"
   },
   {
-    "revision": "c2dfb29263818e1c26b8",
-    "url": "/monsters-rolodex/static/js/main.3912a9d9.chunk.js"
+    "revision": "f365428c553241d58ad1",
+    "url": "/monsters-rolodex/static/js/main.70bc981a.chunk.js"
   },
   {
     "revision": "f56f2e2c6d96b014d91f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/monsters-rolodex/precache-manifest.ddb5bdf4f56c8ec46c7a73e8d14f6f56.js"
+  "/monsters-rolodex/precache-manifest.805a1959d4ec875ad2fd412f5d72f7b7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/monsters-rolodex/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
